d8a0d9bce0a6b66c208dda262e12f7190eec5e3a
Lee Owen <lee_owen@uhaul.com>
2020-10-21 12:27:23 -0700

    Implementing the Mediator Pattern

--- MediatorDemo/Program.cs
+++ MediatorDemo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using MediatorDemo.Structural;
 
 namespace MediatorDemo
 {
@@ -6,7 +6,16 @@ namespace MediatorDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var mediator = new ConcreteMediator();
+
+            var c1 = new Colleague1(mediator);
+            var c2 = new Colleague2(mediator);
+
+            mediator.Colleague1 = c1;
+            mediator.Colleague2 = c2;
+
+            c1.Send("Hello, World! (from c1)");
+            c2.Send("Hello, World! (from c2)");
         }
     }
 }
---
+++ MediatorDemo/Structural/Colleague1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorDemo.Structural
+{
+    public class Colleague1 : Colleague
+    {
+        public Colleague1(Mediator mediator) : base(mediator)
+        {
+
+        }
+
+        public override void HandleNotification(string message)
+        {
+            Console.WriteLine($"Colleague1 receives notification message: {message}");
+        }
+    }
+}
